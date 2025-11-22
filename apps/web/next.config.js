fcf90ef2ee486da9b243c204b109690567c15ae7
popemistress <dheard66@gmail.com>
2025-11-22 16:15:47 +0000

    removed all developer data from codebase and databse

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,52 +1,22 @@
-import { fileURLToPath } from "url";
-import createJiti from "jiti";
 import { env } from "next-runtime-env";
 import { configureRuntimeEnv } from "next-runtime-env/build/configure.js";
-
-// Import env files to validate at build time. Use jiti so we can load .ts files in here.
-createJiti(fileURLToPath(import.meta.url))("./src/env");
 
 configureRuntimeEnv();
 
 /** @type {import("next").NextConfig} */
 const config = {
-  output:
-    env("NEXT_PUBLIC_USE_STANDALONE_OUTPUT") === "true"
-      ? "standalone"
-      : undefined,
+  output: "standalone",
   reactStrictMode: true,
-
-  /** Enables hot reloading for local packages without a build step */
-  transpilePackages: [
-    "@kan/api",
-    "@kan/db",
-    "@kan/shared",
-    "@kan/auth",
-    "@kan/stripe",
-  ],
-
-  /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
         protocol: "https",
         hostname: "*.googleusercontent.com",
       },
     ],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
   },
 
   async rewrites() {
